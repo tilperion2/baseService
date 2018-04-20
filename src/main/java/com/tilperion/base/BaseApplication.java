a6afbaf83ba940a44fd722b6c853de5731c22844
Andrei Merenkov <amerenkov@exadel.com>
2018-04-20 16:11:12 +0300

    test message

--- src/main/java/com/tilperion/base/BaseApplication.java
+++ src/main/java/com/tilperion/base/BaseApplication.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class BaseApplication {
 
 	public static void main(String[] args) {
+//test message
 		SpringApplication.run(BaseApplication.class, args);
 	}
 }

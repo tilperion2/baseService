81c102058a671bcf1585307595c4fda946d2efd6
Andrei Merenkov <amerenkov@exadel.com>
2018-04-20 16:41:42 +0300

    feature change base in head

--- src/main/java/com/tilperion/base/BaseApplication.java
+++ src/main/java/com/tilperion/base/BaseApplication.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class BaseApplication {
 
 	public static void main(String[] args) {
-		//really back?
+		//feature change
 		SpringApplication.run(BaseApplication.class, args);
 	}
 }
